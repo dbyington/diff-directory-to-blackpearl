79a299997bec42b47b9557f6c9039ce2967dc3e5
Don Byington <donby@blacknblue.com>
2016-10-03 11:23:59 -0600

    Add an exclude for the .snapshot directory Update ds3 sdk and gradle versions

--- src/main/java/com/spectra/v2bpverify/Main.java
+++ src/main/java/com/spectra/v2bpverify/Main.java
@@ -9,10 +9,15 @@ import com.spectralogic.ds3client.models.bulk.Ds3Object;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(final String[] args) throws IOException {
+
+        final String excludeString = "/.snapshot/";
 
         final Ds3Client ds3Client = Ds3ClientBuilder.fromEnv().withHttps(false).build();
         final Ds3ClientHelpers clientHelpers = Ds3ClientHelpers.wrap(ds3Client);
@@ -20,11 +25,12 @@ public class Main {
         System.out.println("Getting local file list");
         final Iterable<Ds3Object> localFiles = clientHelpers.listObjectsForDirectory(Paths.get(args[0]));
 
+        System.out.println("Converting file list and stripping matches to: '" + excludeString + "'");
+        final Set<Ds3Object> localSet = convertToSet(localFiles, excludeString);
+
         System.out.println("Getting black pearl file list");
         final Iterable<Contents> bpfilelist = clientHelpers.listObjects(args[1]);
         final Iterable<Ds3Object> bpDs3Objects = clientHelpers.toDs3Iterable(bpfilelist);
-
-        final Set<Ds3Object> localSet = convertToSet(localFiles);
 
         System.out.println("Comparing...");
         difference(localSet, bpDs3Objects);
@@ -41,11 +47,17 @@ public class Main {
         bpDs3Objects.forEach(localSet::remove);
     }
 
-    private static Set<Ds3Object> convertToSet(final Iterable<Ds3Object> localFiles) {
+    private static Set<Ds3Object> convertToSet(final Iterable<Ds3Object> localFiles, final String exclude) {
         final Set<Ds3Object> returnSet = new HashSet<>();
 
-        localFiles.forEach(returnSet::add);
+        //strip the exclude pattern in the process of converting.
+        localFiles.forEach(localFile->{
+            if(!localFile.getName().contains(exclude)) {
+                returnSet.add(localFile);
+            }
+        });
 
         return returnSet;
     }
+
 }
