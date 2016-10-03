30512375eee10f68164c3d32bc2a2530b496a8f2
Don Byington <donby@blacknblue.com>
2016-10-03 15:34:16 -0600

    fixed the exclude string removed unneeded imports

--- src/main/java/com/spectra/diff_directory_to_blackpearl/Main.java
+++ src/main/java/com/spectra/diff_directory_to_blackpearl/Main.java
@@ -9,15 +9,12 @@ import com.spectralogic.ds3client.models.bulk.Ds3Object;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Main {
     public static void main(final String[] args) throws IOException {
 
-        final String excludeString = "/.snapshot/";
+        final String excludeString = ".snapshot/";
 
         final Ds3Client ds3Client = Ds3ClientBuilder.fromEnv().withHttps(false).build();
         final Ds3ClientHelpers clientHelpers = Ds3ClientHelpers.wrap(ds3Client);
